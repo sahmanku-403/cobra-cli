04650440b31ae490edd9c5a544229d44ea1bf7e1
Mayank Kumar <111089357+sahmanku-403@users.noreply.github.com>
2024-08-28 06:40:20 +0530

    Update licenses.go

--- cmd/licenses.go
+++ cmd/licenses.go
@@ -1,17 +1,4 @@
-// Copyright © 2021 Steve Francia <spf@spf13.com>.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-// Parts inspired by https://github.com/ryanuber/go-license
 
 package cmd
 
@@ -27,10 +14,6 @@ import (
 // Licenses contains all possible licenses a user can choose from.
 var Licenses = make(map[string]License)
 
-// License represents a software license agreement, containing the Name of
-// the license, its possible matches (on the command line as given to cobra),
-// the header to be used with each file on the file's creating, and the text
-// of the license
 type License struct {
 	Name            string   // The type of license in use
 	PossibleMatches []string // Similar names to guess
